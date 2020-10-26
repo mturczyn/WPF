c2bb41e5f47a950d341e68a1c66e757f21880312
Michal Turczyn <turek1992@o2.pl>
2020-10-26 22:38:08 +0100

    Dodano przyklad pokazujacy jak zmieniac properrtke, która była zawładnięta przez animacje

--- MvvmTutorial/MvvmTutorial/WpfSamples/Animations.xaml.cs
+++ MvvmTutorial/MvvmTutorial/WpfSamples/Animations.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media;
 
 namespace MvvmTutorial.WpfSamples
 {
@@ -8,5 +10,18 @@ namespace MvvmTutorial.WpfSamples
     public partial class Animations
     {
         public Animations() => InitializeComponent();
+
+        private void FirstButtonColorAnimation_Completed(object sender, System.EventArgs e)
+        {
+            Debug.WriteLine("Setting first button to black background");
+            FirstButton.Background = Brushes.Black;
+        }
+
+        private void SecondButtonColorAnimation_Completed(object sender, System.EventArgs e)
+        {
+            Debug.WriteLine("Setting second button to black background");
+            SecondButtonStroyboard.Remove(SecondButton);
+            SecondButton.Background = Brushes.Black;
+        }
     }
 }
